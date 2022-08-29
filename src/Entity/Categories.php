d210a183162b9e318aacac8df2a9931655647c3d
damirdine <damirdine@outlook.fr>
2022-08-29 15:06:33 +0200

    db migration

---
+++ src/Entity/Categories.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Categories
+ *
+ * @ORM\Table(name="categories")
+ * @ORM\Entity
+ */
+class Categories
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_category", type="smallint", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idCategory;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle_category", type="string", length=100, nullable=false)
+     */
+    private $libelleCategory;
+
+
+}
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -2,19 +2,69 @@
 
 namespace App\Entity;
 
-use App\Repository\TaskRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: TaskRepository::class)]
+/**
+ * Task
+ *
+ * @ORM\Table(name="task", indexes={@ORM\Index(name="category_id", columns={"category_id"})})
+ * @ORM\Entity
+ */
 class Task
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_task", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idTask;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name_task", type="string", length=75, nullable=false)
+     */
+    private $nameTask;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description_task", type="text", length=65535, nullable=false)
+     */
+    private $descriptionTask;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_date_task", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $createdDateTask = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="due_date_task", type="datetime", nullable=false)
+     */
+    private $dueDateTask;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="priority_task", type="string", length=30, nullable=false)
+     */
+    private $priorityTask;
+
+    /**
+     * @var \Categories
+     *
+     * @ORM\ManyToOne(targetEntity="Categories")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="category_id", referencedColumnName="id_category")
+     * })
+     */
+    private $category;
+
+
 }
